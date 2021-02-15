0585653e853dd0e5676aba1d17f7eb7b9aa1298d
DIALLO Thierno Mamadou <thiernomdou63@gmail.com>
2021-02-15 21:55:32 +0100

    AffichageModal

---
+++ js/app.js
@@ -0,0 +1,34 @@
+let modal = null;
+
+const openModal = function(e) {
+    e.preventDefault();
+    const target = document.querySelector(e.target.getAttribute('href'));
+    target.style.display = null;
+    target.removeAttribute('aria-hidden');
+    target.setAttribute('aria-modal', 'true');
+
+    modal = target;
+    modal.addEventListener('click', closeModal);
+    modal.querySelector('.js-modal-close').addEventListener('click', closeModal);
+    modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation);
+}
+
+const closeModal = function(e) {
+    if(modal === null) return
+    e.preventDefault();
+    modal.style.display = "none";
+    target.setAttribute('aria-hidden','true');
+    target.removeAttribute('aria-modal');
+    modal.removeEventListener('click', closeModal);
+    modal.querySelector('.js-modal-close').removeEventListener('click', closeModal);
+    modal.querySelector('.js-modal-stop').removeEventListener('click', stopPropagation);
+    modal = null;
+}
+
+const stopPropagation = function(e) {
+    e.stopPropagation();
+}
+
+document.querySelectorAll('.js-modal').forEach(a => {
+    a.addEventListener('click', openModal)
+});
